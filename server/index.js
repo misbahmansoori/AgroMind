2417ee5309a58d44462fe501e8236e9d6f77461f
Misbah <misbahmansoori14@gmail.com>
2026-07-22 16:19:19 +0530

    MongoDB setup

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Test Route
 app.get("/", (req, res) => {
-    res.send("AgroMind API Running");
+    res.send("🌱 AgroMind API is Running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
